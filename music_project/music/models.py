db694621e3c4d4131f3728026b89b6fc167c824f
Aluno <you@example.com>
2018-11-06 12:01:48 -0200

    create models Banda, Musico and EstiloMusical

--- music_project/music/models.py
+++ music_project/music/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Banda(models.Model):
+	"""docstring for Banda"""
+	nome = models.TextField()
+	data_de_criacao = models.DateField(auto_now = True)
+	numero_de_integrantes = models.BigIntegerField()
+	banda_choices = (
+		(0,'Em atividade'),
+		(1,'Banda encerrada')
+	)
+	em_atividade = models.BigIntegerField(choices = banda_choices, default = 0)
+		
+
+class Musico(models.Model):
+	"""docstring for Musico"""
+
+	nome = models.TextField()
+	data_de_nascimento = models.DateField(auto_now = True)
+	anos_de_carreira = models.BigIntegerField()
+
+class EstiloMusical(models.Model):
+	"""docstring for EstiloMusical"""
+	nome = models.TextField()
+	surgimento = models.DateField(auto_now = True)
+		
+
+		
+		
