ac9f1e51f0772f2a74636e787cec7bf5a5ddcd0f
Aluno <analuizatrz@gmail.com>
2018-11-06 12:12:10 -0200

    create foreign keys

--- music_project/music/models.py
+++ music_project/music/models.py
@@ -2,19 +2,21 @@ from django.db import models
 
 class Banda(models.Model):
 	"""docstring for Banda"""
+	musicos = models.ManyToManyField(Musico)
+	estilo = models.ForeignKey(EstiloMusical, on_delete=models.CASCADE)
+
 	nome = models.TextField()
 	data_de_criacao = models.DateField(auto_now = True)
-	numero_de_integrantes = models.BigIntegerField()
+	numero_de_integrantes = models.IntegerField()
 	banda_choices = (
 		(0,'Em atividade'),
 		(1,'Banda encerrada')
 	)
-	em_atividade = models.BigIntegerField(choices = banda_choices, default = 0)
+	em_atividade = models.IntegerField(choices = banda_choices, default = 0)
 		
 
 class Musico(models.Model):
 	"""docstring for Musico"""
-
 	nome = models.TextField()
 	data_de_nascimento = models.DateField(auto_now = True)
 	anos_de_carreira = models.BigIntegerField()
