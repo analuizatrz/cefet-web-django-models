04a242f439af39d8b949063a67312bbd3c6c3cbc
analuizatrz <analuizatrz@gmail.com>
2018-11-12 12:38:34 -0200

    create Banda and __str__ methods for model classes

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # cefet-web-django-models
 Prática de Programação web
+
+from music.models import *
+from datetime import date
+grunge = EstiloMusical.objects.create(nome="Grunge", surgimento=date(1980,1,1))
+layne = Musico.objects.create(nome="Layne Staley", data_de_nascimento=date(1967,8,22), anos_de_carreira=23)
+mike = Musico.objects.create(nome="Mike Starr", data_de_nascimento=date(1966,4,4),anos_de_carreira=30)
+
+
+```sudo apt-get install python3-pip```
+
+sudo apt-get install python3 pyhton3-pip
+
+pip3 install django
+
+python3 manage.py makemigrations
+
+python3 manage.py migrate
+
+python3 manage.py shell
+
--- music_project/music/models.py
+++ music_project/music/models.py
@@ -1,9 +1,26 @@
 from django.db import models
 
+class Musico(models.Model):
+	"""docstring for Musico"""
+	nome = models.TextField()
+	data_de_nascimento = models.DateField(auto_now = True)
+	anos_de_carreira = models.BigIntegerField()
+
+	def __str__(self):
+		return "{nome} ({data_de_nascimento}): {anos_de_carreira} anos de carreira".format(nome = self.nome, data_de_nascimento = self.data_de_nascimento, anos_de_carreira = self.anos_de_carreira)
+
+class EstiloMusical(models.Model):
+	"""docstring for EstiloMusical"""
+	nome = models.TextField()
+	surgimento = models.DateField(auto_now = True)
+
+	def __str__(self):
+		return "{nome} ({surgimento})".format(nome = self.nome, surgimento = self.surgimento)
+		
 class Banda(models.Model):
 	"""docstring for Banda"""
 	musicos = models.ManyToManyField(Musico)
-	estilo = models.ForeignKey(EstiloMusical, on_delete=models.CASCADE)
+	estilo = models.ForeignKey(EstiloMusical, on_delete=models.CASCADE, related_name='User', null=True)
 
 	nome = models.TextField()
 	data_de_criacao = models.DateField(auto_now = True)
@@ -13,19 +30,6 @@ class Banda(models.Model):
 		(1,'Banda encerrada')
 	)
 	em_atividade = models.IntegerField(choices = banda_choices, default = 0)
-		
 
-class Musico(models.Model):
-	"""docstring for Musico"""
-	nome = models.TextField()
-	data_de_nascimento = models.DateField(auto_now = True)
-	anos_de_carreira = models.BigIntegerField()
-
-class EstiloMusical(models.Model):
-	"""docstring for EstiloMusical"""
-	nome = models.TextField()
-	surgimento = models.DateField(auto_now = True)
-		
-
-		
-		
+	def __str__(self):
+		return "{nome} \ncriada em {data_de_criacao} \nEstilo: {estilo} \nMusicos: {musicos}".format(nome=self.nome, data_de_criacao=self.data_de_criacao, estilo=self.estilo, musicos = ", ".join([m for m in self.musicos]))
